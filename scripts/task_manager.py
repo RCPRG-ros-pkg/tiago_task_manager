20fc4c8fd1750eea68df5f1772e61b9255d848a2
Dawid Seredyński <dawid.seredynski@gmail.com>
2019-12-03 12:16:32 +0100

    support for MoveTo and Wander actions

--- scripts/task_manager.py
+++ scripts/task_manager.py
@@ -41,10 +41,89 @@ import copy
 
 import tf
 import tf2_ros
+from tf.transformations import quaternion_from_euler
+import actionlib
+from geometry_msgs.msg import Pose
+
 from tiago_msgs.msg import Command
 
+from rosplan_tiago_scenarios_msgs.msg import GoAction, GoActionGoal, GoActionFeedback, GoActionResult, GoGoal
+
+from tiago_behaviours_msgs.msg import WanderAction, WanderGoal
+
+def deg2rad(angle_deg):
+    return float(angle_deg)/180.0*math.pi
+
+def makePose(x, y, theta):
+    q = quaternion_from_euler(0, 0, theta)
+    result = Pose()
+    result.position.x = x
+    result.position.y = y
+    result.orientation.x = q[0]
+    result.orientation.y = q[1]
+    result.orientation.z = q[2]
+    result.orientation.w = q[3]
+    return result
+
 def taskGoTo(place_name):
-    pass
+    print 'Poproszono mnie o przejscie do: ' + place_name
+    # TODO: transform place_name to pose
+    if place_name == 'kuchnia':
+        pose = makePose(3, 0.2, -math.pi/2)
+    elif place_name in ['warsztat', 'warsztatu']:
+        pose = makePose(1.55, 8.65, math.pi/2)
+    elif place_name in ['pokoj', 'pok\\303\\263j']:
+        pose = makePose(-0.15, -0.3, math.pi/2)
+    elif place_name == 'sypialnia':
+        pose = makePose(3, 5, math.pi/2)
+    else:
+        print 'Nie wiem gdzie jest: ' + place_name
+        return False
+
+    # Creates the SimpleActionClient, passing the type of the action to the constructor.
+    client = actionlib.SimpleActionClient('go', GoAction)
+
+    # Waits until the action server has started up and started
+    # listening for goals.
+    client.wait_for_server()
+
+    # Creates a goal to send to the action server.
+    goal = GoGoal()
+
+    goal.pose = pose
+
+    #goal.header.stamp = rospy.get_rostime()
+    #goal.header.frame_id = 'map'
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
+    # Waits for the server to finish performing the action.
+    client.wait_for_result()
+
+    # Prints out the result of executing the action
+    return client.get_result()
+
+def taskWander():
+    print 'Poproszono mnie, abym zaczal patrolowac'
+    # Creates the SimpleActionClient, passing the type of the action to the constructor.
+    client = actionlib.SimpleActionClient('wander', WanderAction)
+
+    # Waits until the action server has started up and started
+    # listening for goals.
+    client.wait_for_server()
+
+    # Creates a goal to send to the action server.
+    goal = WanderGoal()
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
+    # Waits for the server to finish performing the action.
+    client.wait_for_result()
+
+    # Prints out the result of executing the action
+    return client.get_result()
 
 def taskBring(object_name):
     pass
@@ -62,12 +141,13 @@ def callback(data):
         print 'poproszono mnie o przyniesienie: ' + object_name
 
     elif data.intent_name == 'projects/incare-dialog-agent/agent/intents/0165eceb-9621-4a7d-aecc-7a879951da18':
-        place_name = param_dict['miejsce']
-        print 'poproszono mnie o przejscie do: ' + place_name
+        taskGoTo( param_dict['miejsce'] )
 
     elif data.intent_name == 'projects/incare-dialog-agent/agent/intents/2f028022-05b6-467d-bcbe-e861ab449c17':
         print 'Niezrozumiale polecenie: "' + data.query_text + '"'
 
+    elif data.intent_name == 'projects/incare-dialog-agent/agent/intents/d9e96166-030b-442f-a513-d3fa2e044030':
+        taskWander()
     else:
         raise Exception('Unknown intent: "' + data.intent_name + '", query_text: "' + data.query_text + '"')
 
